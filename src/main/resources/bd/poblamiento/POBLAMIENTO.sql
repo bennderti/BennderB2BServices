085a0377aab4742e0523baab3bb19659a3a731d6
bennderti <bennder.ti@gmail.com>
2017-05-07 21:46:22 -0300

    - Tipos de beneficio

--- src/main/resources/bd/poblamiento/POBLAMIENTO.sql
+++ src/main/resources/bd/poblamiento/POBLAMIENTO.sql
@@ -408,3 +408,12 @@ INSERT INTO PROVEEDOR.COMUNA(ID_COMUNA,ID_REGION,NOMBRE) VALUES(15201,15,'Putre'
 INSERT INTO PROVEEDOR.COMUNA(ID_COMUNA,ID_REGION,NOMBRE) VALUES(15202,15,'General Lagos');
 
 
+
+/*==============================================================*/
+/* Tipos de beneficio    */
+/*==============================================================*/
+insert into tipo_beneficio values(1,'Descuento');
+insert into tipo_beneficio values(2,'Producto');
+insert into tipo_beneficio values(3,'Adicional');
+
+
